d8b68c4fe9b6532ace894ee7c77e367a34148e4e
anshifa riya <anshifariya042@gmail.com>
2026-04-07 11:53:32 +0530

    baseurl fixed  in frontend authservice

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api/auth`,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
