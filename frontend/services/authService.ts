6f1db61e03fb77127faabb2c5e75db0e3fca30d5
anshifa riya <anshifariya042@gmail.com>
2026-04-07 14:26:09 +0530

    remove the duplicate auth

--- frontend/services/authService.ts
+++ frontend/services/authService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/auth`, 
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`, 
   withCredentials: true,
 });
 
